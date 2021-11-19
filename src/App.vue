e7541d060f5e80c89bb4a4a7ee6e766dc8a229bc
Poonnatuch B <6110613228@student.tu.ac.th>
2021-11-19 15:42:15 +0700

    Fix router path

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,6 @@
 <script>
 
 export default {
-  name: 'App',
 
-  data: () => ({
-    //
-  }),
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../components/Home'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +8,8 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: { title: 'Home' }
   },
   {
     path: '/about',
@@ -16,7 +17,8 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../components/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../components/About.vue'),
+    meta: { title: 'About' }
   }
 ]
 
