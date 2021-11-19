a31a7aa2df3f08a6a1c8f60d121d934689df2937
Poonnatuch B <6110613228@student.tu.ac.th>
2021-11-19 16:34:47 +0700

    Delete Home.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <v-app>
+  <v-app style="background-color: black">
+    <v-app-bar color="yellow darken-1" app>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-title>App</v-app-bar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-
-export default {
-
-};
+export default {};
 </script>
