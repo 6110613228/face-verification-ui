8626f90f28bb43357db6bc39361f5cec34641185
Poonnatuch B <6110613228@student.tu.ac.th>
2021-11-19 16:35:10 +0700

    Add Regis, Veri, Welcome components

---
+++ src/components/Welcome.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container class="white--text" fill-height>
+    <v-row>
+      <v-col>
+        <v-btn block x-large>Registration</v-btn>
+      </v-col>
+      <v-col>
+        <v-btn block x-large>Face verification</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Welcome",
+
+  data() {
+    return {};
+  },
+};
+</script>
