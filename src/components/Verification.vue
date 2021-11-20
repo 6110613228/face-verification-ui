ad538c8fa50d675d54c21d31bab6f54b8ac6bf81
Poonnatuch B <6110613228@student.tu.ac.th>
2021-11-20 10:29:36 +0700

    Get user's media device

--- src/components/Verification.vue
+++ src/components/Verification.vue
@@ -1,15 +1,42 @@
 <template>
   <v-container class="white--text">
-      Verification
+    <v-row>
+      <v-col>
+        <video id="camera"></video>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      ImageCapture: null,
+    };
+  },
+  mounted() {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: { facingMode: "user" },
+        audio: false,
+      })
+      .then((stream) => {
+        const track = stream.getVideoTracks()[0];
+        try {
+          this.ImageCapture = new ImageCapture(track);
+          let camera = document.getElementById("camera");
+          camera.srcObject = stream;
+          camera.play();
+        } catch (err) {
+          console.log("No image capture feature");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
