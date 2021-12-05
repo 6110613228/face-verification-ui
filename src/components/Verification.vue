7030ae1fd6cef648619959ed890d5b0ae541bcdf
Poonnatuch B <6110613228@student.tu.ac.th>
2021-12-05 20:51:25 +0700

    Change URL

--- src/components/Verification.vue
+++ src/components/Verification.vue
@@ -188,7 +188,7 @@ export default {
       this.ctx.stroke();
     },
     webSocketInit() {
-      this.webSocket = new WebSocket("ws://localhost:8000/ws");
+      this.webSocket = new WebSocket("wss://2d25-171-7-38-133.ngrok.io/ws");
 
       this.webSocket.onopen = () => {
         console.log("Connection opened.");
