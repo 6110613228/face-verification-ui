a911820213411b2e377d110045fc339571a86cf2
Poonnatuch B <6110613228@student.tu.ac.th>
2021-12-07 16:35:03 +0700

    Change URL

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -605,7 +605,7 @@ export default {
         formData.append("label", this.label);
 
         axios
-          .post("https://a77c-171-7-38-133.ngrok.io/register", formData, {
+          .post("https://d308-171-7-38-133.ngrok.io/register", formData, {
             headers: {
               "Content-Type": "multipart/form-data",
             },
--- src/components/Verification.vue
+++ src/components/Verification.vue
@@ -199,7 +199,7 @@ export default {
       this.ctx.stroke();
     },
     webSocketInit() {
-      this.webSocket = new WebSocket("wss://a77c-171-7-38-133.ngrok.io/ws");
+      this.webSocket = new WebSocket("wss://d308-171-7-38-133.ngrok.io/ws");
 
       this.webSocket.onopen = () => {
         console.log("Connection opened.");
