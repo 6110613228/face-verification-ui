6a127b4dbe8cca90d8e34e2da2f9a158b49b68ba
Poonnatuch B <6110613228@student.tu.ac.th>
2021-12-07 12:09:02 +0700

    Update About

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,15 +1,39 @@
 <template>
   <v-container>
-      About
+    <h1>Project FaceCN</h1>
+    <h2>Team</h2>
+    <ul>
+      <li>Wanachai</li>
+      <li>Packawat</li>
+      <li>Poonnatuch Boonyarattanasoontorn</li>
+      <li>Yodsatorn Pantongkam</li>
+      <li>Rathapol Phutharaksa</li>
+    </ul>
+    <h2>Github</h2>
+    <ul>
+      <li>
+        <a
+          href="https://github.com/6110613228/face-verification-ui"
+          target="_blank"
+          rel="noopener noreferrer"
+          >Frontend(VueJS)</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/6110613228/face-verification-backend"
+          target="_blank"
+          rel="noopener noreferrer"
+          >Backend(Fastapi, WebSocket, Tensorflow)</a
+        >
+      </li>
+    </ul>
+    <v-img src="../assets/Logo_ECE.png" max-width="350" contain></v-img>
   </v-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
